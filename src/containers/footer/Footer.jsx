bf937265ea52de81601e65e77a62b9f8d166ba36
Gleb Vorontsov <wehaveto95@gmail.com>
2023-02-18 16:14:51 +0200

    Footer_commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer =() => {
     return (
-            <div>
-                <h1>Footer</h1>
+            <div className="gpt3__footer section__padding">
+                <div className="gpt3__footer-heading">
+                    <h1 className="gradient__text">Do you want to step in to the future before others?</h1>
+                </div>
+                <div className="gpt3__footer-btn">
+                    <p>Request early Access</p>
+                </div>
+
+                <div className="gpt3__footer-links">
+                    <div className="gpt3__footer-links_logo">
+                        <img src={gpt3Logo} alt="logo"/>
+                        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Links</h4>
+                        <p>Overons</p>
+                        <p>Social media</p>
+                        <p>Counters</p>
+                        <p>Contact</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Company</h4>
+                        <p>Terms & Conditions</p>
+                        <p>Privacy Policy</p>
+                        <p>Contact</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Get in Touch</h4>
+                        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+                        <p>00-53324342-432</p>
+                        <p>info@example.com</p>
+                    </div>
+                </div>
+                    <div className="gpt3__footer-copyright">
+                        <p>@2021 GPT-3. All rights reserved.</p>
+                    </div>
+
             </div>
             )
 }
